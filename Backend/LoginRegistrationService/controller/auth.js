0f01b2c06c82bb3d1a91324ecc37fd38a3935e33
xLyaso <61823523+xLyaso@users.noreply.github.com>
2020-11-14 01:59:45 +0100

    Backend for LoginRegistration, mysql connection still needs work.

---
+++ Backend/LoginRegistrationService/controller/auth.js
@@ -0,0 +1,42 @@
+const { validationResult } = require('express-validator');
+
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
+
+exports.register = async  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) return;
+
+    const vorname = req.body.vorname;
+    const nachname = req.body.nachname;
+    const email = req.body.email;
+    const passwort = req.body.passwort;
+    const geburtstag = req.body.geburtstag;
+
+    try
+    {
+        const hashedPasswort = await bcrypt.hash(passwort, 12);
+        const userDetails = {
+            vorname: vorname,
+            nachname: nachname,
+            email: email,
+            passwort: hashedPasswort,
+            geburtstag: geburtstag,
+        };
+
+        const result = await User.save(userDetails);
+
+        res.status(201).json({ message: 'Benutzer registriert!'});
+
+    }
+    catch (err)
+    {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+
+};
