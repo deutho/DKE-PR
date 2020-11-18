ebc5cf7f942ab7fbde1a879062c20e91a9524864
Thomas <t_deutsch@gmx.at>
2020-11-18 16:37:22 +0100

    CSS copy link to clipboard and overlay

--- Frontend/src/app/services/userService.ts
+++ Frontend/src/app/services/userService.ts
@@ -13,28 +13,8 @@ export class userService
     
     constructor(private httpclient: HttpClient) {}
 
-    // getAllHTTP(): Observable<any> {
-    //     return this.httpclient.get(apiurl);
-    // }
-
-
-    // async getByShortURLHTTP(shortURL:string): Promise<Observable<any>> {
-    //     //var param = (<HTMLInputElement>document.getElementById("sURL")).value;
-    //     return await this.httpclient.get(apiurl+shortURL);
-    // }
-
-
-    // postURLHTTP(toPost:URL): Observable<any> {
-    //     return this.httpclient.post(apiurl, toPost);
-    // }
-
-    // deleteURLHTTP(shortURL:string): Observable<any>{
-    //     return this.httpclient.delete(apiurl+shortURL);
-    // }
-
     registerUser(user:user): Observable<any> {
-        console.log("in user Service: ", user)
-        return this.httpclient.post(apiurl, user);
+        return  this.httpclient.post(apiurl, user);
     }
 
 
